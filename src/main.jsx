25993ab02d19b1445ca75cbbee05dbe2fa17415f
shandraprabhu <shandraprabhu@gmail.com>
2025-10-08 12:13:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")).render(
     {/* <App /> */}
     {/* <TwoWayBinding></TwoWayBinding> */}
   
-   </TimerComponent>
-    {/*  <Changer/>   */}
+   <Changer/>
+    {/* </TimerComponent>    */}
   </StrictMode>
 );
